b6b3e92f3ab1ac6f92af82245ba9b212a9031927
Cookiescooker <fhhdhysd@gmail.com>
2024-09-14 16:41:35 +0700

    Update user.model.js

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,20 +1,47 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    require: [true, "Name is required"]
-  },
-  email: {
-    type: String,
-    require: [true, "Email is required"],
-    unique: true,
-    lowercase: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    require: [true, "Password is required"],
-    minlength: [6, "Password must be atleast 6 characters long"]
-  }
-})
+const userSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, "Name is required"],
+		},
+		email: {
+			type: String,
+			required: [true, "Email is required"],
+			unique: true,
+			lowercase: true,
+			trim: true,
+		},
+		password: {
+			type: String,
+			required: [true, "Password is required"],
+			minlength: [6, "Password must be at least 6 characters long"],
+		},
+		cartItems: [
+			{
+				quantity: {
+					type: Number,
+					default: 1,
+				},
+				product: {
+					type: mongoose.Schema.Types.ObjectId,
+					ref: "Product",
+				},
+			},
+		],
+		role: {
+			type: String,
+			enum: ["customer", "admin"],
+			default: "customer",
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
